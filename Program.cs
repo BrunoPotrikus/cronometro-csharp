332b49d1442fe5dd8d5c65940578d6190cbb4871
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2022-12-21 15:08:02 -0300

    Cronômetro com função de contagem, menu de escolha e tratamento dos dados informados pelo usuário

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.WriteLine("S = Segundo => 10s = 10 segundos");
+        Console.WriteLine("M = Minuto => 1m = 1 minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Informe quanto tempo deseja contar");
+
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring((data.Length - 1), 1));
+        Console.WriteLine(data);
+        Console.WriteLine(type);
+    }
+
+    static void Start(int time)
+    {
+        int currentTime = 0;
+
+        while(currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
+
+        Console.Clear();
+        Console.WriteLine("Cronômetro finalizado.");
+        Thread.Sleep(2500);
+    }
+}
